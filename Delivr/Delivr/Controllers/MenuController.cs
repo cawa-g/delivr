3dd82933bbd5db31f73fc5bc796f556c7ff945db
unknown <AM46560@logti-a3324-07.ens.ad.etsmtl.ca>
2015-06-12 10:06:27 -0400

    Redid all I lost from not pushing earlier...

--- Delivr/Delivr/Controllers/MenuController.cs
+++ Delivr/Delivr/Controllers/MenuController.cs
@@ -32,7 +32,7 @@ namespace Delivr.Controllers
 
         public ActionResult BlankMenuItemRow()
         {
-            return View("MenuItemRow", new EditMenuItemModel());
+            return PartialView("MenuItemRow", new EditMenuItemModel());
         }
 
         //
@@ -43,25 +43,40 @@ namespace Delivr.Controllers
             if (restaurantId == null)
                 return HttpNotFound();
 
-            Menu menu = db.Menus.SingleOrDefault(m => m.RestaurantId == restaurantId);
+            Restaurant restaurant = db.Restaurants.Find(restaurantId);
+            if (restaurant == null || (!User.IsInRole("Admin") && restaurant.UserId != WebSecurity.CurrentUserId))
+                return HttpNotFound();
 
+            Menu menu = db.Menus.SingleOrDefault(m => m.RestaurantId == restaurantId);
             EditMenuModel model;
             if (menu != null)
             {
-                model = new EditMenuModel(menu);
+                List<EditMenuItemModel> menuItemModels = new List<EditMenuItemModel>();
+                foreach (MenuItem item in menu.MenuItems)
+                {
+                    menuItemModels.Add(new EditMenuItemModel()
+                    {
+                        MenuItemId = item.MenuItemId,
+                        Nom = item.Nom,
+                        Description = item.Description,
+                        Prix = new Decimal(item.Prix) / 100
+                    });
+                }
+
+                model = new EditMenuModel()
+                {
+                    MenuId = menu.MenuId,
+                    Nom = menu.Nom,
+                    RestaurantId = menu.RestaurantId,
+                    MenuItemModels = menuItemModels
+                };
             }
             else
             {
-                Restaurant restaurant = db.Restaurants.Find(restaurantId);
-
-                if (restaurant != null && (User.IsInRole("Admin") || restaurant.UserId == WebSecurity.CurrentUserId))
+                model = new EditMenuModel()
                 {
-                    model = new EditMenuModel(restaurantId.Value);
-                }
-                else
-                {
-                    return HttpNotFound();
-                }
+                    RestaurantId = restaurantId.Value
+                };
             }
 
             return View(model);
@@ -76,38 +91,65 @@ namespace Delivr.Controllers
         {
             if (ModelState.IsValid)
             {
-                /*db.Entry(menu).State = EntityState.Modified;
+                List<MenuItem> menuItems = new List<MenuItem>();
+                if (model.MenuItemModels != null)
+                {
+                    foreach (EditMenuItemModel itemModel in model.MenuItemModels)
+                    {
+                        MenuItem item;
+                        if (itemModel.MenuItemId.HasValue)
+                        {
+                            item = db.MenuItems.Find(itemModel.MenuItemId.Value);
+                            item.Nom = itemModel.Nom;
+                            item.Description = itemModel.Description;
+                            item.Prix = Decimal.ToInt32(itemModel.Prix * 100);
+                        }
+                        else
+                        {
+                            item = new MenuItem()
+                            {
+                                Nom = itemModel.Nom,
+                                Description = itemModel.Description,
+                                Prix = Decimal.ToInt32(itemModel.Prix * 100)
+                            };
+
+                            db.MenuItems.Add(item);
+                        }
+
+                        menuItems.Add(item);
+                    }
+                }
+
+                Menu menu;
+                if (model.MenuId.HasValue)
+                {
+                    menu = db.Menus.Find(model.MenuId.Value);
+
+                    // Iterate through the items of the stored menu:
+                    foreach (MenuItem item in menu.MenuItems.ToList())
+                    {
+                        // If the item does not exist in the presentation model, it has been removed:
+                        if (!model.MenuItemModels.Any(itemModel => itemModel.MenuItemId.HasValue && itemModel.MenuItemId.Value == item.MenuItemId))
+                            db.MenuItems.Remove(item);
+                    }
+                }
+                else    // New menu - no need to check for items to delete
+                {
+                    menu = new Menu()
+                    {
+                        Nom = model.Nom,
+                        RestaurantId = model.RestaurantId,
+                        MenuItems = menuItems
+                    };
+
+                    db.Menus.Add(menu);
+                }
+
                 db.SaveChanges();
-                return RedirectToAction("Index");*/
+                ViewBag.SuccessMessage = "Définition du menu complétée avec succès.";
             }
 
             return View(model);
-        }
-
-        //
-        // GET: /Menu/Delete/5
-
-        public ActionResult Delete(int id = 0)
-        {
-            Menu menu = db.Menus.Find(id);
-            if (menu == null)
-            {
-                return HttpNotFound();
-            }
-            return View(menu);
-        }
-
-        //
-        // POST: /Menu/Delete/5
-
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Menu menu = db.Menus.Find(id);
-            db.Menus.Remove(menu);
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
