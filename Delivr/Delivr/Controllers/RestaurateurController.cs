e20db4c986c147d14929c4678515c6a09f230409
Simon <simt2@hotmail.com>
2015-06-12 04:15:42 -0400

    ajout de create restaurateur avec selection de resto

--- Delivr/Delivr/Controllers/RestaurateurController.cs
+++ Delivr/Delivr/Controllers/RestaurateurController.cs
@@ -39,6 +39,23 @@ namespace Delivr.Controllers
 
         public ActionResult Create()
         {
+            List<SelectListItem> restaurants = new List<SelectListItem>();
+            restaurants.Add(new SelectListItem
+            {
+                Value = null,
+                Text = "",
+            });
+            foreach (Restaurant r in db.Restaurants.ToList())
+            {
+             
+                restaurants.Add(new SelectListItem
+                {
+                    Value = r.RestaurantId.ToString(),
+                    Text = r.nom,
+                });
+            }
+            ViewBag.DropDownRestaurants = restaurants;
+            
             return View();
         }
 
@@ -47,16 +64,31 @@ namespace Delivr.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(Restaurateur restaurateur)
+        public ActionResult Create(CreateModel model)
         {
-            if (ModelState.IsValid)
-            {
+            Restaurateur restaurateur = new Restaurateur();
+            //if (ModelState.IsValid)
+            //{
+                restaurateur.CodeCivique = model.CodeCivique;
+                restaurateur.CodePostale = model.CodePostale;
+                restaurateur.DateNaissance = model.DateNaissance;
+                restaurateur.Email = model.Email;
+                restaurateur.Nom = model.Nom;
+                restaurateur.Prenom = model.Prenom;
+                restaurateur.Rue = model.Rue;
+                restaurateur.Telephone = model.Telephone;
+                restaurateur.UserName = model.Email;
+                if (model.restaurantId == null)
+                {
+                    return RedirectToAction("Message", "Restaurateur", "Le restaurateur à été ajouté sans restaurant");
+                }
+                restaurateur.Restaurants.Add(db.Restaurants.Find(model.restaurantId));
                 db.UserProfiles.Add(restaurateur);
                 db.SaveChanges();
                 return RedirectToAction("Index");
-            }
+            //}
 
-            return View(restaurateur);
+            return View(model);
         }
 
         //
@@ -112,6 +144,17 @@ namespace Delivr.Controllers
             db.UserProfiles.Remove(restaurateur);
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        //
+        // GET: /Account/Message
+
+
+        [ValidateInput(false)]
+        public ActionResult Message(string chaine)
+        {
+            ViewBag.Chaine = chaine;
+            return View();
         }
 
         protected override void Dispose(bool disposing)
--- Delivr/Delivr/Models/RestaurateurModels.cs
+++ Delivr/Delivr/Models/RestaurateurModels.cs
@@ -16,4 +16,60 @@ namespace Delivr.Models
         }
         public virtual ICollection<Restaurant> Restaurants { get; set; }
     }
+
+    public class CreateModel
+    {
+        [Required]
+        [Display(Name = "Adresse courriel")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Adresse courriel invalide")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "La chaîne {0} doit comporter au moins {2} caractères.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mot de passe")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmer le mot de passe ")]
+        [Compare("Password", ErrorMessage = "Le mot de passe et le mot de passe de confirmation ne correspondent pas.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Prénom")]
+        public string Prenom { get; set; }
+
+        [Required]
+        [Display(Name = "Nom")]
+        public string Nom { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Téléphone invalide, utilisez le format xxx-xxx-xxxx")]
+        [Display(Name = "Téléphone")]
+        public string Telephone { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "La chaîne {0} doit comporter au moins {2} caractères.", MinimumLength = 3)]
+        [Display(Name = "Rue")]
+        public string Rue { get; set; }
+
+        [Required]
+        [Display(Name = "Code Civique")]
+        public int CodeCivique { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Z]\d[A-Z] \d[A-Z]\d$", ErrorMessage = "Code postale invalide, utilisez le format H1H 1H1")]
+        [Display(Name = "Code Postale")]
+        [DataType(DataType.PostalCode)]
+        public string CodePostale { get; set; }
+
+        [Required]
+        [Display(Name = "Date de naissance")]
+        [DataType(DataType.Date, ErrorMessage = "Date invalide, utilisez le format JJ/MM/AAAA"), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime DateNaissance { get; set; }
+
+        public int? restaurantId { get; set; }
+    }
 }
